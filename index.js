03937a39feb7140bb1c94ae517cd22599f4444b4
rodnei0 <rodnei0@gmail.com>
2022-01-28 13:31:09 -0400

    post/participants and get/participants done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import joi from "joi";
 import { MongoClient } from "mongodb";
 
 const app = express();
@@ -10,14 +11,33 @@ app.use(cors());
 dotenv.config();
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+const participantSchema = joi.object({
+    name: joi.string().required()
+  });
+
 app.post("/participants", async (req, res) => {
     const name = req.body;
+    const validation = participantSchema.validate(name, { abortEarly: true });
+
+    if (validation.error) {
+        console.log(validation.error.details);
+        res.status(422).send("Nome do usuário não pode ser uma string vazia!");
+        mongoClient.close()
+    }
+
     try {
 		await mongoClient.connect();
 		const db = mongoClient.db(process.env.MONGO_NAME);
 		const participantsCollection = db.collection(process.env.MONGO_PARTICIPANTS);
+
+        const participant = await participantsCollection.findOne({ name: name });
+        if (participant) {
+            res.status(409).send("Nome de usuário já existe!");
+            mongoClient.close()
+        }
+
         await participantsCollection.insertOne({name: name, lastStatus: Date.now()});
-		res.status(201).send("deu bom");
+		res.sendStatus(201);
 		mongoClient.close()
 	 } catch (error) {
 	    res.status(500).send('A culpa foi do estagiário')
